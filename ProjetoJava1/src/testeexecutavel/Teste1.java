1a57677bb79a1e64069a9575f450d875f446cdaa
Guilherme Cardoso <guilherme_wa@hotmail.com>
2021-08-20 18:02:31 -0300

    v1

---
+++ ProjetoJava1/src/testeexecutavel/Teste1.java
@@ -0,0 +1,10 @@
+package testeexecutavel;
+
+public class Teste1 {
+
+	/*main é um método auto executavel em Java*/
+	public static void main(String[] args) {
+		System.out.println("meu primeiro programa em Java");
+		System.out.println("teste 2");
+	}
+}
